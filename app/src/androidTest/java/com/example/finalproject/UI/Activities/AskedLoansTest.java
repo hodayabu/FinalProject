76a1f41a2602e4dbf4fa82da5af5ff167a91e238
hodayabu <bhodaya@videocites.com>
2020-05-11 01:44:04 +0300

    unit tests

---
+++ app/src/androidTest/java/com/example/finalproject/UI/Activities/AskedLoansTest.java
@@ -0,0 +1,39 @@
+package com.example.finalproject.UI.Activities;
+
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.finalproject.R;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class AskedLoansTest {
+
+    @Rule
+    public ActivityTestRule<AskedLoans> mActivityTestRule = new ActivityTestRule<>(AskedLoans.class);
+    private AskedLoans mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+
+    @Test
+    public void onCreate() {
+        View view = mActivity.findViewById(R.id.askedLoansList);
+        assertNotNull(view);
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+}
